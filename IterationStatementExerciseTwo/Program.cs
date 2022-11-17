b2c313471dbeead0eea954a8d2536ac5ba6f3eda
Caleb Ricks <99217033+Skultanix@users.noreply.github.com>
2022-11-17 15:46:50 -0700

    Part two completed.

--- IterationStatementExerciseTwo/Program.cs
+++ IterationStatementExerciseTwo/Program.cs
@@ -1,10 +1,31 @@
-﻿namespace IterationStatementExerciseTwo
+﻿using System.Xml.Schema;
+
+namespace IterationStatementExerciseTwo
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var total = 0;
+
+            while(true)
+            {
+                
+
+                Console.WriteLine(@"Please type a number or type ""ok"" to exit.");
+                var userInput = Console.ReadLine();
+
+                if (userInput == "ok")
+                {
+                    Console.WriteLine("Program terminated.");
+                    break;
+                } else
+                {
+                    var newInput = int.Parse(userInput);
+                    total = newInput + total;
+                    Console.WriteLine(string.Format("Total of numbers entered is: {0}.", total));
+                }
+            }
         }
     }
 }
